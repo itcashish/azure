a3cb7737c8e478e9bc418fbae8aadbbec2f3c22d
Ashish Patel <itcashish@gmail.com>
2020-02-17 14:58:38 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import pymysql
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World! from Azure"
+
+@app.route("/con")
+def con();
+    conn = pymysql.connect(host='my01.winhost.com', user='ashishwp', password='patelashwp',db='mysql_19668_wp1')
+    cu = conn.cursor()
+    sql =("select * from stockmast")
+    cu.execute(sql)
+    data = cu.fetchone()
+    return data
