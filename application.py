4c37118bbc416f8bcdcceeccf60f77459b51ef43
Ashish Patel <itcashish@gmail.com>
2020-02-17 15:02:37 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! from Azure"
+    return "Hello World! from Azure check mysql"
 
 @app.route("/con")
-def con();
+def con():
     conn = pymysql.connect(host='my01.winhost.com', user='ashishwp', password='patelashwp',db='mysql_19668_wp1')
     cu = conn.cursor()
     sql =("select * from stockmast")
